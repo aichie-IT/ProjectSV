91d0340aad0fd316f03cea505eceb6e874cd1e4b
aichie-IT <s22a0051@siswa.umk.edu.my>
2026-01-03 23:58:42 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -6,9 +6,9 @@ st.set_page_config(page_title="Students Mental Health Analysis", layout="wide")
 home = st.Page("home.py", title="Home", icon=":material/home:")
 
 internetUsage = st.Page("Nur_Aishah_Sakinah.py", title="Internet Use vs. Mental Health", icon=":material/insights:")
-ilya = st.Page("ilya.py", title="Advanced Visualizations", icon=":material/show_chart:")
-hanis = st.Page("hanis.py", title="Correlation Insights", icon=":material/share:")
-ainun = st.Page("ainun.py", title="Riding Behavior Insights", icon=":material/pedal_bike:")
+ilya = st.Page("Ilya.py", title="Advanced Visualizations", icon=":material/show_chart:")
+hanis = st.Page("Hanis_Nabila.py", title="Correlation Insights", icon=":material/share:")
+ainun = st.Page("Ainun.py", title="Riding Behavior Insights", icon=":material/pedal_bike:")
 
 # Sidebar Navigation
 pg = st.navigation({
