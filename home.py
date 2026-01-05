0a8350a694f4bf99a80bc87bdcfbcd786896310f
aichie-IT <s22a0051@siswa.umk.edu.my>
2026-01-05 12:49:45 +0800

    Update home.py

--- home.py
+++ home.py
@@ -106,16 +106,19 @@ st.markdown("---")
 # --- SUMMARY BOX ---
 col1, col2, col3, col4 = st.columns(4)
 
+top_academic = df['General_Academic_Performance'].mode()[0]
+top_media = df['Social_Media_Use_Frequency'].mode()[0]
+
 if not filtered_df.empty:
-    col1.metric("Total Records", f"{len(filtered_df):,}", help="PLO 1: Total Respondent Records of Student", border=True)
-    col2.metric("Avg. Age", f"{filtered_df['Age'].mean():.1f} years", help="PLO 2: Students Age", border=True)
-    col3.metric("Avg. Positive Impact", f"{filtered_numeric['Social_Media_Positive_Impact_on_Wellbeing_Numeric'].mean():.1f}", help="PLO 3: Positive Impact on Wellbeing", border=True)
-    col4.metric("Avg. Negative Impact", f"{filtered_numeric['Social_Media_Negative_Impact_on_Wellbeing_Numeric'].mean():.1f}", help="PLO 4: Negative Impact on Wellbeing", border=True)
+    col1.metric("Total Records", f"{len(df):,}", help="PLO 1: Total Respondent Records of Student", border=True)
+    col2.metric("Avg. Age", f"{df['Age'].mean():.1f} years", help="PLO 2: Students Age", border=True)
+    col3.metric("Academic Performance", top_academic, help="PLO 3: Students Academic Performance", border=True)
+    col4.metric("Social Media Usage", top_media, help="PLO 4: Social Media Use Frecuency", border=True)
 else:
     col1.metric("Total Records", "0", help="No data available")
     col2.metric("Avg. Age", "N/A", help="No data available")
-    col3.metric("Avg. Positive Impact", "N/A", help="No data available")
-    col4.metric("Avg. Negative Impact", "N/A", help="No data available")
+    col3.metric("Academic Performance", "N/A", help="No data available")
+    col4.metric("Social Media Usage", "N/A", help="No data available")
 
 st.markdown("---")
 
